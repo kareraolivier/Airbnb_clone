919dc7c186727b30a766f34866f415cda9c6f558
kareraolivier <kareraolivier@gmail.com>
2023-07-12 12:26:16 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        home: "url('./dist/asset/61EC3Je+XaL._SX3000_.jpg')",
-        "footer-texture": "url('/img/footer-texture.png')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
